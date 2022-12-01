fd6bc2f828230cf9216cc814e0d5d66f1d1fa045
Ange Tran <ange.tran@health.vic.gov.au>
2022-12-01 13:45:05 +1100

    added main.js file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("adding new main.js file")
+
+function angesWorld() {
+    //this is an alert for when the codingBoss function is called
+alert("Welcome to my world!");
+}
+angesWorld();
