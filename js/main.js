a279de7184459014859685138f28164a302dc128
Ange Tran <ange.tran@health.vic.gov.au>
2022-12-06 13:30:44 +1100

    updating js console log

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 console.log("adding new main.js file")
 
 function angesWorld() {
-    //this is an alert for when the codingBoss function is called
+    //this is an alert for when the angesWorld function is called
 alert("Welcome to my world!");
 }
 angesWorld();
+
+console.log("changed heading banner on main page")
